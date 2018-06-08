1928de0cc6cac04698e871586f4eea6342608905
Eren,Sinan (BI X) BIX-DE-I <sinan.eren@boehringer-ingelheim.com>
2018-06-08 11:18:57 +0200

    Add spinner and default back to empty parameters

--- error-handling-demo/ShowUserInfoViewController.swift
+++ error-handling-demo/ShowUserInfoViewController.swift
@@ -14,14 +14,16 @@ final class ShowUserInfoViewController: UIViewController {
     @IBOutlet private weak var pressHereButton: UIButton!
     @IBOutlet private weak var nameLabel: UILabel!
     @IBOutlet private weak var ageLabel: UILabel!
-
+    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+    
     private let showUserInfoViewModel = ShowUserInfoViewModel()
     private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         pressHereButton.rx.controlEvent(.touchUpInside)
+            .delay(2.0, scheduler: MainScheduler.instance)
             .bind(to: showUserInfoViewModel.input.triggerPressHere)
             .disposed(by: disposeBag)
 
@@ -32,7 +34,24 @@ final class ShowUserInfoViewController: UIViewController {
         showUserInfoViewModel.output.userName
             .drive(nameLabel.rx.text)
             .disposed(by: disposeBag)
+        
+        showUserInfoViewModel.output.hideActivityIndicator
+            .startWith(true)
+            .drive(activityIndicator.rx.isHidden)
+            .disposed(by: disposeBag)
 
+        showUserInfoViewModel.output.animateActivityIndicator
+            .drive(activityIndicator.rx.isAnimating)
+            .disposed(by: disposeBag)
+        
+        showUserInfoViewModel.output.isReset
+            .observeOn(MainScheduler())
+            .delay(4.0, scheduler: MainScheduler.instance)
+            .subscribe(onNext: { _ in
+                self.nameLabel.text = User.empty._name
+                self.ageLabel.text = String(describing: User.empty._age) 
+            }).disposed(by: disposeBag)
+        
         showUserInfoViewModel.output.error.asObservable()
             .filter { $0 != .none }
             .observeOn(MainScheduler())
--- error-handling-demo/ShowUserInfoViewModel.swift
+++ error-handling-demo/ShowUserInfoViewModel.swift
@@ -25,13 +25,29 @@ final class ShowUserInfoViewModel: ShowUserInfoViewModelType {
     var triggerPressHereSubject = PublishSubject<Void>()
 
     struct Input {
+        // Observing any changes in button click events
         let triggerPressHere: AnyObserver<Void>
     }
 
     struct Output {
+        
+        // Displays user age upon user signal
         let userAge: Driver<String>
+        
+        // Displays user name upon user signal
         let userName: Driver<String>
+        
+        // Hides activity indicator before the network call and after object loading
+        let hideActivityIndicator: Driver<Bool>
+        
+        // Animates the activity indicator in between the network call and object loading
+        let animateActivityIndicator: Driver<Bool>
+        
+        // Displays error upon an error signal
         let error: Observable<CustomError>
+        
+        // Reset the output user information
+        let isReset: Observable<Bool>
     }
 
     init() {
@@ -43,17 +59,35 @@ final class ShowUserInfoViewModel: ShowUserInfoViewModelType {
 
         let triggeredUserData = triggerPressHereSubject
             .flatMapLatest { userObservable }
+        
+        let hasFetched = triggeredUserData.map { _ in true }
+            .asDriver(onErrorJustReturn: true)
 
         let user = triggeredUserData.map { $0.value ?? .empty }
         let userName = user.map { $0._name }
         let userAge = user.map { "\($0._age)" }
         let error = triggeredUserData.map { $0.error ?? .none }
+        
+        let showSpinner = triggerPressHereSubject.map { _ in false }
+            .asDriver(onErrorJustReturn: false)
+        let isError = error
+            .filter { $0 != .none }
+            .map { _ in true }
+            .asDriver(onErrorJustReturn: true)
+        let hideActivityIndicator = Driver.of(hasFetched, isError, showSpinner)
+            .merge()
+        let animateActivityIndicator = hideActivityIndicator.map { !$0 }
 
+        let resetSignal = user.map { _ in true }
+        
         input = Input(triggerPressHere: triggerPressHereSubject.asObserver())
         output = Output(
             userAge: userAge.asDriver(onErrorJustReturn: ""),
             userName: userName.asDriver(onErrorJustReturn: ""),
-            error: error
+            hideActivityIndicator: hideActivityIndicator,
+            animateActivityIndicator: animateActivityIndicator,
+            error: error,
+            isReset: resetSignal
         )
     }
 }
